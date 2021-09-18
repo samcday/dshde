a6d56d027b4d8db9a61774be1c5d5ef1e01bff54
Sam Day <me@samcday.com>
2021-09-18 13:31:24 +0200

    down.sh

---
+++ down.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set -ueo pipefail
+
+until ! hcloud server describe dev-env >/dev/null 2>&1; do
+  hcloud volume detach dev-env >/dev/null 2>&1 || true
+  hcloud server delete dev-env >/dev/null 2>&1 || true
+done
