a832ef3f97c74d7a8fc9a85162574598a92e6e56
Sam Day <me@samcday.com>
2021-09-19 17:44:03 +0200

    marginally better workon.sh

--- workon.sh
+++ workon.sh
@@ -2,13 +2,13 @@
 set -ueo pipefail
 
 if [[ ! -f /.devenv ]]; then
-  exec hcloud server ssh dev-env -A ~/dshde/workon.sh "$*"
+  exec hcloud server ssh dev-env -t -A '~/dshde/workon.sh' "$*"
 fi
 
 mkdir -p /mnt/src/$1
 
 if ! docker inspect $1 >/dev/null 2>&1; then
-  docker run --runtime=sysbox-runc -d --name $1 -v /mnt/src/$1:/work dev-env-image /sbin/init
+  docker run --runtime=sysbox-runc -d --name $1 -h $1 -v /mnt/src/$1:/work dev-env-image /sbin/init
 fi
 
 exec docker exec -it -u dev -w /work $1 /bin/bash
