12bc4278bf4d874ab0bcf982fa367c7811f8faed
Sam Day <me@samcday.com>
2021-10-13 10:30:02 +0200

    more graceful down.sh

--- down.sh
+++ down.sh
@@ -1,5 +1,23 @@
 #!/usr/bin/env bash
 set -ueo pipefail
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+if hcloud server describe dev-env >/dev/null 2>&1; then
+  ssh -T -F ssh_config root@$(hcloud server ip dev-env) <<'CLEANUP'
+for vm in $(lxc-ls --running); do
+  echo stopping $vm
+  lxc-stop $vm
+done
+
+loginctl kill-user dev
+umount /var/lib/lxc || true
+umount /mnt || true
+CLEANUP
+fi
+
+if [[ -f .state/ip ]]; then
+  ssh -F ssh_config -O exit {root,dev}@$(cat .state/ip) || true
+fi
 
 until ! hcloud server describe dev-env >/dev/null 2>&1; do
   hcloud volume detach dev-env >/dev/null 2>&1 || true
